b50bfc3a104e337e3cc18704769b5e0d1c7320af
naroso_o <naroso1751@naver.com>
2025-07-13 04:53:43 +0900

    feat: error path page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import { Button } from '../lib';
+</script>
+
+<div class="max-w-4xl mx-auto px-4 py-8">
+	<div class="flex flex-col items-center justify-center gap-8">
+		<h1 class="text-center text-2xl font-bold">{page.status} {page.error?.message}</h1>
+		<Button href="/" variant="primary">돌아가기</Button>
+	</div>
+</div>
+
+<style>
+	h1 {
+		font-size: 2rem;
+		font-weight: bold;
+		color: #333;
+	}
+</style>
